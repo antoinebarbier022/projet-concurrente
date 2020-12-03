018a4ec50c9f3f3087ae577f510716c217c28db2
Antoine Barbier <emilebarbier022@gmail.com>
2020-12-03 09:52:02 +0100

    On recommence ... on fait une version 2 du projet

---
+++ src/centralise-version2/client.c
@@ -0,0 +1,17 @@
+#include "struct.h"
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ src/centralise-version2/struct.h
@@ -0,0 +1,5 @@
+#ifndef H_GL_STRUCT
+#define H_GL_STRUCT
+
+
+#endif
